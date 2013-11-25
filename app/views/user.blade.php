c7633cb40715baae556d05329e9402f418370447
Andżelo Mazurek <andre2go@gmail.com>
2013-11-25 08:03:45 +0100

    Add Back link for user view

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -1,6 +1,10 @@
 @extends('layout')
 
 @section('content')
+    <p>
+      <a href="{{ URL::route('users') }}">&laquo; Back to Team page</a>
+    </p>
+
     <img class="avatar img-thumbnail" src="{{asset('avatars')}}/{{ (isset($user->avatar)) ? $user->avatar : 'avatar.png' }}" alt="{{ $user->name }}" />
 
     <h1>{{ $user->name }}</h1>
